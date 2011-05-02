d57571ad532c226d39b5617ca4ae2c9efd42064c
stitounajlaa@gmail.com <stitounajlaa@gmail.com@866b98a2-4e70-6576-87ff-457e3cb233d7>
2011-05-02 14:39:14 +0000

    All listeners supported

--- branches/mvp4j-impl/src/org/mvp4j/impl/swing/JListActionComponent.java
+++ branches/mvp4j-impl/src/org/mvp4j/impl/swing/JListActionComponent.java
@@ -2,17 +2,37 @@ package org.mvp4j.impl.swing;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+import java.awt.event.ContainerEvent;
+import java.awt.event.ContainerListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.HierarchyBoundsListener;
+import java.awt.event.HierarchyEvent;
+import java.awt.event.HierarchyListener;
+import java.awt.event.InputMethodEvent;
+import java.awt.event.InputMethodListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
-import javax.swing.JComboBox;
+
 import javax.swing.JList;
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import javax.swing.event.PopupMenuEvent;
+import javax.swing.event.PopupMenuListener;
 
 import org.apache.log4j.Logger;
 import org.mvp4j.adapter.ActionBinding;
@@ -27,6 +47,18 @@ public class JListActionComponent extends ActionComponent{
 	private MouseListener mouseListener;
 	private KeyListener keyListener;
 	private ListSelectionListener listSelectionListener;
+	private ComponentListener componentListener;
+	private AncestorListener ancestorListener;
+	private ContainerListener containerListener;
+	private FocusListener focusListener;
+	private HierarchyBoundsListener hierarchyBoundsListener;
+	private HierarchyListener hierarchyListener;
+	private InputMethodListener inputMethodListener;
+	private MouseMotionListener mouseMotionListener;
+	private MouseWheelListener mouseWheelListener;
+
+	private PropertyChangeListener propertyChangeListener;
+	
 		private Logger logger = LoggerUtils.getLogger();
 	private String eventAction;
 	
@@ -119,9 +151,9 @@ public class JListActionComponent extends ActionComponent{
 					
 				}
 			};
-			jlist.addMouseListener(mouseListener);
-	    }
+			jlist.addMouseListener(mouseListener);}
 	    
+	
 	    else if(eventType==KeyListener.class){
 		  
 		  keyListener=new KeyListener() {
@@ -159,9 +191,257 @@ public class JListActionComponent extends ActionComponent{
 		jlist.addKeyListener(keyListener);
 		
 	  }
-	    else{
-	  logger.info("This event type "+eventType+" is not supported");
-	    }
+	   
+	    else if(eventType==ComponentListener.class){
+			componentListener=new ComponentListener() {
+				
+				@Override
+				public void componentShown(ComponentEvent e) {
+					if(eventAction.equals("componentShown")){
+						actionBinding.callAction(e);
+						
+					}
+					
+					
+				}
+				
+				@Override
+				public void componentResized(ComponentEvent e) {
+					if(eventAction.equals("componentResized")){
+						actionBinding.callAction(e);
+						
+					}
+					
+					
+				}
+				
+				@Override
+				public void componentMoved(ComponentEvent e) {
+					if(eventAction.equals("componentMoved")){
+						actionBinding.callAction(e);
+						
+					}
+					
+				}
+				
+				@Override
+				public void componentHidden(ComponentEvent e) {
+					if(eventAction.equals("componentHidden")){
+						actionBinding.callAction(e);
+						
+					}
+					
+					
+				}
+			};
+			jlist.addComponentListener(componentListener);
+		}
+		else if(eventType==AncestorListener.class){
+			ancestorListener=new AncestorListener() {
+				
+				@Override
+				public void ancestorRemoved(AncestorEvent e) {
+					if(eventAction.equals("ancestorRemoved")){
+						actionBinding.callAction(e);
+						
+					}
+					
+					
+				}
+				
+				@Override
+				public void ancestorMoved(AncestorEvent e) {
+					if(eventAction.equals("ancestorMoved")){
+						actionBinding.callAction(e);
+						
+					}
+					
+					
+				}
+				
+				@Override
+				public void ancestorAdded(AncestorEvent e) {
+					if(eventAction.equals("ancestorAdded")){
+						actionBinding.callAction(e);
+						
+					}
+					
+					
+				}
+			};
+			jlist.addAncestorListener(ancestorListener);
+		}
+		else if(eventType==ContainerListener.class){
+			containerListener=new ContainerListener() {
+				
+				@Override
+				public void componentRemoved(ContainerEvent e) {
+					if(eventAction.equals("componentRemoved")){
+						actionBinding.callAction(e);
+						
+					}
+					
+				}
+				
+				@Override
+				public void componentAdded(ContainerEvent e) {
+					if(eventAction.equals("componentAdded")){
+						actionBinding.callAction(e);
+						
+					}
+					
+				}
+			};
+			jlist.addContainerListener(containerListener);
+		}
+		else if(eventType==FocusListener.class){
+			focusListener=new FocusListener() {
+				
+				@Override
+				public void focusLost(FocusEvent e) {
+					if(eventAction.equals("focusLost")){
+						actionBinding.callAction(e);
+						
+					}
+					
+				}
+				
+				@Override
+				public void focusGained(FocusEvent e) {
+					if(eventAction.equals("focusGained")){
+						actionBinding.callAction(e);
+						
+					}
+					
+				}
+			};
+			jlist.addFocusListener(focusListener);
+		}
+		else if(eventType==HierarchyBoundsListener.class){
+			hierarchyBoundsListener=new HierarchyBoundsListener() {
+				
+				@Override
+				public void ancestorResized(HierarchyEvent e) {
+					if(eventAction.equals("ancestorResized")){
+						actionBinding.callAction(e);
+						
+					}
+					
+				}
+				
+				@Override
+				public void ancestorMoved(HierarchyEvent e) {
+					if(eventAction.equals("ancestorMoved")){
+						actionBinding.callAction(e);
+						
+					}
+					
+				}
+			};
+			jlist.addHierarchyBoundsListener(hierarchyBoundsListener);
+		}
+		else if(eventType==HierarchyListener.class){
+			if(eventAction.equals("")){
+				eventAction="HierarchyListener";
+			}
+			hierarchyListener=new HierarchyListener() {
+				
+				@Override
+				public void hierarchyChanged(HierarchyEvent e) {
+					if(eventAction.equals("hierarchyChanged")){
+						actionBinding.callAction(e);
+						
+					}
+					
+				}
+			};
+			jlist.addHierarchyListener(hierarchyListener);
+		}
+		else if(eventType==InputMethodListener.class){
+			inputMethodListener=new InputMethodListener() {
+				
+				@Override
+				public void inputMethodTextChanged(InputMethodEvent e) {
+					if(eventAction.equals("inputMethodTextChanged")){
+						actionBinding.callAction(e);
+						
+					}
+					
+				}
+				
+				@Override
+				public void caretPositionChanged(InputMethodEvent e) {
+					if(eventAction.equals("caretPositionChanged")){
+						actionBinding.callAction(e);
+						
+					}
+					
+				}
+			};
+			jlist.addInputMethodListener(inputMethodListener);
+		}
+		else if(eventType==MouseMotionListener.class){
+			mouseMotionListener=new MouseMotionListener() {
+				
+				@Override
+				public void mouseMoved(MouseEvent e) {
+					if(eventAction.equals("mouseMoved")){
+						actionBinding.callAction(e);
+						
+					}
+					
+				}
+				
+				@Override
+				public void mouseDragged(MouseEvent e) {
+					if(eventAction.equals("mouseDragged")){
+						actionBinding.callAction(e);
+						
+					}
+					
+				}
+			};
+			jlist.addMouseMotionListener(mouseMotionListener);
+		}
+		else if(eventType==MouseWheelListener.class){
+			if(eventAction.equals("")){
+				eventAction="mouseWheelMoved";
+			}
+			mouseWheelListener=new MouseWheelListener() {
+				
+				@Override
+				public void mouseWheelMoved(MouseWheelEvent e) {
+					if(eventAction.equals("mouseWheelMoved")){
+						actionBinding.callAction(e);
+						
+					}
+					
+				}
+			};
+			jlist.addMouseWheelListener(mouseWheelListener);
+		}
+		
+		else if(eventType==PropertyChangeListener.class){
+			if(eventAction.equals("")){
+				eventAction="propertyChange";
+			}
+			propertyChangeListener= new PropertyChangeListener() {
+				
+				@Override
+				public void propertyChange(PropertyChangeEvent e) {
+					if(eventAction.equals("propertyChange")){
+						actionBinding.callAction(e);
+						
+					}
+					
+				}
+			};
+			jlist.addPropertyChangeListener(propertyChangeListener);
+		}
+		else{
+			logger.info("This event type "+eventType+" is not supported");
+	}
+
 	}
 
 	@Override

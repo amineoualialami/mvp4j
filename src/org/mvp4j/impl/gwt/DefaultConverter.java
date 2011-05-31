19e054beda94b91c12215037777dd0850a67d0f5
amine.oualialami@supinfo.com <amine.oualialami@supinfo.com@866b98a2-4e70-6576-87ff-457e3cb233d7>
2011-05-31 14:26:52 +0000

    Modification API , converter.convertComponentToModel(Object type, Object value)

--- src/org/mvp4j/impl/gwt/DefaultConverter.java
+++ src/org/mvp4j/impl/gwt/DefaultConverter.java
@@ -5,30 +5,31 @@ import java.util.Collection;
 import org.mvp4j.Converter;
 
 import com.google.gwt.core.client.GWT;
+import com.gwtent.reflection.client.TypeOracle;
 
 public class DefaultConverter implements Converter {
 
 	@Override
-	public Object convertComponentToModel(Class<?> type, Object value) {
+	public Object convertComponentToModel(Object type, Object value) {
 		if (value instanceof String) {
-			if (type == Integer.class)
+			if (type == TypeOracle.Instance.getClassType(Integer.class))
 				return new Integer((String) value);
-			else if (type == int.class)
+			else if (type == TypeOracle.Instance.getClassType(int.class))
 				return new Integer((String) value);
-			else if (type == float.class)
+			else if (type == TypeOracle.Instance.getClassType(float.class))
 				return new Float((String) value);
-			else if (type == Float.class)
+			else if (type == TypeOracle.Instance.getClassType(Float.class))
 				return new Float((String) value);
-			else if (type == String.class)
+			else if (type == TypeOracle.Instance.getClassType(String.class))
 				return value.toString();
-			else if (type == double.class)
+			else if (type == TypeOracle.Instance.getClassType(double.class))
 				return new Double((String) value);
-			else if (type == Double.class)
+			else if (type == TypeOracle.Instance.getClassType(Double.class))
 				return new Double((String) value);
 
 //			logger.error("The type : " + type.getName()
 //					+ " is not supported with the value of type String");
-			GWT.log("The type : " + type.getName()
+			GWT.log("The type : " + type.toString()
 					+ " is not supported with the value of type String");
 			throw new IllegalArgumentException();
 		} else if (value instanceof Object)

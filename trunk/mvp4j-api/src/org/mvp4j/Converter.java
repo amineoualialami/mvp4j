febc5788f4a7c3603c1ba4570a328ad83d15780c
amine.oualialami@supinfo.com <amine.oualialami@supinfo.com@866b98a2-4e70-6576-87ff-457e3cb233d7>
2012-01-09 21:53:57 +0000

    MVP4J API 2.0

---
+++ trunk/mvp4j-api/src/org/mvp4j/Converter.java
@@ -0,0 +1,34 @@
+package org.mvp4j;
+
+import org.mvp4j.adapter.MVPAdapter;
+
+/**
+ * <p>
+ * The converter class to convert an object value to an instance of specified type.
+ * <p>
+ * Use this to set a specific converter by calling {@link AppController#setConverter(Converter)}
+ * <p>
+ * The {@link AppController} will use the converter to convert component value before updating the associated
+ * model property value.
+ * <p>
+ * When developing an adapter class {@link MVPAdapter}, create a customized converter depending 
+ * on the adapter and return it in the method {@link MVPAdapter#getConvertor()}
+ * 
+ *
+ */
+public interface Converter {
+	/**
+	 * Convert an object value to an instance of specified type. 
+	 * 
+	 * If is not possible to convert throw a new {@link IllegalArgumentException}
+	 * 
+	 * @param type the target type of the instance converted from the value object
+	 * @param value the object value to convert
+	 * @return the result of conversion
+	 */
+	public Object convertComponentToModel(Object type, Object value);
+	
+	  // todo javaDoc
+	public Object convertModelToComponent(Object value);
+
+}
---
+++ trunk/mvp4j-api/src/org/mvp4j/adapter/ActionComponent.java
@@ -0,0 +1,91 @@
+package org.mvp4j.adapter;
+
+
+/**
+ * <p>
+ * This class is used to simplify the binding between the event 
+ * of the current component in the view and associated action in the presenter
+ * and simplify add event and action call operations during implementing an adapter class {@link MVPAdapter} to a specific library.
+ * <p>
+ * The user can use the instance actionBinding of type {@link ActionBinding} 
+ * to add an event to the current view component and call the associated action in the current presenter.
+ * 
+ * Example with JButton in Swing Adapter :
+ public class JButtonEventActionComponent extends ActionComponenet {
+
+	private JButton button;
+	private ActionBinding actionBinding;	
+	private static final Class<?> DEFAULT_EVENT_TYPE = ActionListener.class;	
+	private ActionListener listener = null;
+
+	public JButtonEventActionComponent(ActionBinding actionBinding) {
+		super(actionBinding);
+		this.button = (JButton) actionBinding.getComponent();
+		this.eventActionBinding = actionBinding;
+	}
+
+	@Override
+	public void bind() {
+		Class<?> eventType = actionBinding.getEventType();
+		if(eventType == null)
+			eventType = DEFAULT_EVENT_TYPE;
+		if(eventType == ActionListener.class){
+			listener = new ActionListener() {				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					actionBinding.callAction();					
+				}
+			};
+			button.addActionListener( listener);
+		}else
+			throw new IllegalArgumentException("The event type is not supported");
+		
+	}
+	@Override
+	public void unbind() {
+		button.removeActionListener(listener);
+		
+	}
+
+}
+ *
+ */
+public abstract class ActionComponent {
+	
+	/**
+	 * This field is useful to interact with the component and presenter and get all binding informations.
+	 */
+	protected ActionBinding actionBinding;
+	
+	
+	/**
+	 * initialize the actionComponent with the necessary parameters  
+	 * @param actionBinding
+	 */
+	public void init(ActionBinding actionBinding){
+		this.actionBinding=actionBinding;
+	}
+	
+	/**
+	 * Bind the current component with the current action in the presenter
+	 * Add the current type event(or the default event if is not specified) to the current view component 
+	 * and call the presenter action associated when the event is invoked
+	 * if the event type is specified and is not supported, than throw an {@link IllegalArgumentException}
+	 */
+	public abstract void bind();
+	
+	/**
+	 * Unbind the current component with the current presenter action,
+	 * remove the event added by the method bind
+	 */
+	public abstract void unbind();
+
+	/**
+	 * Get the This field to interact with the component and presenter and get all binding informations.
+	 * @return ActionBinding
+	 */
+	public ActionBinding getEventActionBinding() {
+		return actionBinding;
+	}		
+	
+}

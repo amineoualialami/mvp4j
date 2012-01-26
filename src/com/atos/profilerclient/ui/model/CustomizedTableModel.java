002c45395bf0f1455d0555d4beb8ef7e2a953967
stitounajlaa@gmail.com <stitounajlaa@gmail.com@866b98a2-4e70-6576-87ff-457e3cb233d7>
2012-01-26 23:31:19 +0000

    Overriding the correct method

--- src/com/atos/profilerclient/ui/model/CustomizedTableModel.java
+++ src/com/atos/profilerclient/ui/model/CustomizedTableModel.java
@@ -2,20 +2,18 @@ package com.atos.profilerclient.ui.model;
 
 
 
+import java.util.Map;
+
 import org.mvp4j.adapter.ModelBinding;
 import org.mvp4j.impl.swing.JTableModelComponent;
 
 public class CustomizedTableModel extends JTableModelComponent {
 
-	
-	
-
 	@Override
-	public void initModelComponent(ModelBinding modelBinding) {
-		super.initModelComponent(modelBinding);
+	public Object initTableModel(Map<String, String> customizedColumns) {
 		getCustomizedColumns().put("name","name");
 		getCustomizedColumns().put("date","date");
-		initTableModel(getCustomizedColumns());
+		return super.initTableModel(getCustomizedColumns());
 	}
 
 	

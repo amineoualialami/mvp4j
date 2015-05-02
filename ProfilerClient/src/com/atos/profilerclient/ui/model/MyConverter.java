a3f6c42d3f0bcd8fa40ba13aa2bf1f2fcd88c204
amineoualialami <a.oualialami@gmail.com>
2015-05-02 15:40:24 +0000

    qsz

--- ProfilerClient/src/com/atos/profilerclient/ui/model/MyConverter.java
+++ ProfilerClient/src/com/atos/profilerclient/ui/model/MyConverter.java
@@ -1,16 +1,10 @@
 package com.atos.profilerclient.ui.model;
 
-import java.awt.Component;
-import java.awt.Graphics;
-import java.awt.Rectangle;
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyEditor;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import java.util.Date;
-import java.util.logging.SimpleFormatter;
 
 import org.mvp4j.Converter;
 
@@ -25,7 +19,6 @@ public class MyConverter implements Converter {
 			 dateformat=formater.parse(""+value);
 			 dateformat=formater.parse(formater.format(dateformat));
 		} catch (ParseException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		System.out.println(value);
